9c6e7602c44833d25621c0ed0955684e1d84e9e3
yunusemresencan <yunusemresencan0@gmail.com>
2024-02-05 11:39:20 +0300

    Add skeleton for gene prediction endpoint

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,32 @@
-from flask import Flask
-
+import flask.json
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/home")
 def home():
     return "<p>Home Page</p>"
 
+
+#TODO: integrate with uniprot api
+@app.post("/api/gene")
+def gene_post():
+    res = []
+    for i in request.json["geneList"]:
+        gene = i["gene"]
+        positions = i["positions"]
+        for j in positions:
+            #prediction here
+            res.append(
+                {"gene": gene,
+                  "position": j,
+                  "prediction": "Q05655",
+                  "probability": 0.85
+                }
+            )
+
+
+    return flask.json.jsonify(
+        results=res
+    )
 if __name__ == "__main__":
     app.run()

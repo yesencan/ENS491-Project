ba2183fe33b2b8216cf36af319f9f5a11ac802e1
yunusemresencan <yunusemresencan0@gmail.com>
2024-02-05 14:09:11 +0300

    Connect gene endpoint to UniProt API

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,24 +1,26 @@
 import flask.json
 from flask import Flask, request
+import protein_utils
 app = Flask(__name__)
 
 @app.route("/home")
 def home():
     return "<p>Home Page</p>"
 
-
-#TODO: integrate with uniprot api
 @app.post("/api/gene")
 def gene_post():
     res = []
     for i in request.json["geneList"]:
-        gene = i["gene"]
+        gene = i['gene']
+        gene_sequence = protein_utils.fetch_gene_sequence(gene)
         positions = i["positions"]
-        for j in positions:
+        peptides = protein_utils.separate_peptides(gene_sequence, positions)
+        for peptide_record in peptides:
             #prediction here
             res.append(
                 {"gene": gene,
-                  "position": j,
+                 "peptide": peptide_record[0],
+                  "position": peptide_record[1],
                   "prediction": "Q05655",
                   "probability": 0.85
                 }

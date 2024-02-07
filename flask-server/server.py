428b8c00bccd2a35cc21d8392cc131aa38799f18
yunusemresencan <yunusemresencan0@gmail.com>
2024-02-07 16:54:07 +0300

    Add prediction with fasta file upload

--- flask-server/server.py
+++ flask-server/server.py
@@ -30,5 +30,32 @@ def gene_post():
     return flask.json.jsonify(
         results=res
     )
+
+
+@app.post("/api/predict/sequence-file")
+def sequence_post():
+    json_data = flask.json.loads(request.files['json'].read())
+
+    res = []
+
+    fasta_records = protein_utils.parse_fasta(request.files['file'].read().decode('utf-8'))
+    for record in fasta_records:
+        peptides = protein_utils.separate_peptides(str(record.seq))
+        for peptide_record in peptides:
+            #TODO: Run real prediction when model is ready
+            res.append(
+                {"gene": protein_utils.get_uniprot_id(record.id),
+                 "peptide": peptide_record[0],
+                  "position": peptide_record[1],
+                  "prediction": "Q05655",
+                  "probability": 0.85
+                }
+            )
+    
+    return flask.json.jsonify(
+        results=res
+    )
+
+
 if __name__ == "__main__":
     app.run()
